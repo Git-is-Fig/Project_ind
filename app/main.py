58a3199e0eaeef30b1805ed3e8c8f5d0385f0583
Git-is-Fig <117030291+Git-is-Fig@users.noreply.github.com>
2025-05-15 14:46:55 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,14 +1,7 @@
 from fastapi import FastAPI
 
-
 app = FastAPI()
-
 
 @app.get("/")
 def read_root():
     return {"msg": "Hello World"}
-
-
-@app.get("/health")
-def health():
-    return {"status": "ok"}
